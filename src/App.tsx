fb519d1c39b60041c33aa23997ea2d303de3ea16
jilio <jilio.stories@gmail.com>
2021-01-23 18:25:07 +0700

    box & orbit controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { OrbitControls } from "@react-three/drei"
+import { Canvas } from "react-three-fiber"
+import Box from "./Box"
+
 function App() {
-  return <div>hello</div>
+  return (
+    <Canvas
+      style={{ width: 800, height: 800 }}
+      pixelRatio={window.devicePixelRatio ? window.devicePixelRatio : 1}
+    >
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+    </Canvas>
+  )
 }
 
 export default App
